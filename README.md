c9f6a96b7d2d82ce4a14dadcff830c43b4fcf568
nika <nikaonime@gmail.com>
2024-04-26 17:55:06 +0300

    initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+## Dependencies
+
+`react-router-dom` - для маршрутизации
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+    return (
+        <div className="app">
+            <main className="main_wrapper">App</main>
+        </div>
+    );
+};
+
+export default App;
