03d854c25f8d6ccf6ec899f297f75f7324e57426
nika <nikaonime@gmail.com>
2024-04-27 11:04:57 +0300

    pages, Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import Blog from 'pages/Blog';
+import SignUp from 'pages/SignUp';
+import SignIn from 'pages/SignIn';
+import Profile from 'pages/Profile';
 
 const App: FC = () => {
     return (
         <div className="app">
-            <main className="main_wrapper">App</main>
+            <main className="main_wrapper">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/profile/:userId" element={<Profile />} />
+                </Routes>
+            </main>
         </div>
     );
 };
