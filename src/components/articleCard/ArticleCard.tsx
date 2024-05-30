363f03213b04ea4761679560204f8ef1e5d741c3
nika <nikaonime@gmail.com>
2024-05-30 14:49:04 +0300

    adaptive of BlogContent

--- src/components/articleCard/ArticleCard.tsx
+++ src/components/articleCard/ArticleCard.tsx
@@ -5,8 +5,6 @@ import { Timestamp } from 'firebase/firestore';
 import { IArticle } from 'types/articles/articles';
 import { distanceDate } from 'helpers/formatDate';
 
-import typeImg from 'assets/type.jpg';
-
 import styles from './ArticleCard.module.css';
 
 interface ArticleCardProps {
@@ -19,31 +17,31 @@ const ArticleCard: FC<ArticleCardProps> = ({ article }) => {
     );
 
     return (
-        <div className={styles.article}>
-            {article.image && (
-                <img
-                    className={styles.article_img}
-                    src={article.image}
-                    alt="article_img"
-                />
-            )}
-            <div className={styles.distanceTime}>{distanceTime} назад</div>
-            <div className={styles.content}>
-                <h3>
-                    <Link to={`/article/${article.id}`}>{article.name}</Link>
-                </h3>
-                <p className={styles.article_text}>{article.description}</p>
-                <div className={styles.author}>
-                    <div className={styles.info}>
-                        {/* <img src={krisImg} alt="avatar" /> */}
-                        <h4>{article.user.username}</h4>
+        <Link to={`/article/${article.id}`}>
+            <div className={styles.article}>
+                {article.image && (
+                    <img
+                        className={styles.article_img}
+                        src={article.image}
+                        alt="article_img"
+                    />
+                )}
+                <div className={styles.distanceTime}>{distanceTime} назад</div>
+                <div className={styles.content}>
+                    <h3>{article.name}</h3>
+                    <p className={styles.article_text}>{article.description}</p>
+                    <div className={styles.author}>
+                        <div className={styles.info}>
+                            {/* <img src={krisImg} alt="avatar" /> */}
+                            <h4>{article.user.username}</h4>
+                        </div>
+                        <p className={styles.date}>
+                            {article.createdAt.formatedDate}
+                        </p>
                     </div>
-                    <p className={styles.date}>
-                        {article.createdAt.formatedDate}
-                    </p>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
